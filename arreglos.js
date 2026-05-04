84f6aa07571bc8f1e67a73387e142709c9fa7f06
paulveloz28 <paulveloz28@gmail.com>
2026-05-03 20:01:58 -0500

    feat: create a function pintarArregloIzquierda that iterates over the left array

--- arreglos.js
+++ arreglos.js
@@ -7,7 +7,7 @@ function agregarEdad() {
     // obtiene el valor de la caja de texto
     let cajaEdad = document.getElementById("edad");
     let valor = cajaEdad.value;
-    
+
     // convierte a entero
     let edad = parseInt(valor);
 
@@ -16,4 +16,35 @@ function agregarEdad() {
 
     // vuelve a pintar el arreglo izquierdo
     pintarArregloIzquierdo();
+}
+
+function pintarArregloIzquierdo() {
+
+    let tabla = document.getElementById("tablaIzquierda");
+
+    let contenido = "";
+
+    for (let i = 0; i < edadesIzquierda.length; i++) {
+
+        contenido += `
+      <tr>
+         <td>${edadesIzquierda[i]}</td>
+
+            <td>
+                <button onclick="eliminarIzquierdo(${i})"
+                    class="btn-eliminar">
+                    Eliminar
+                </button>
+            </td>
+
+            <td>
+                <button onclick="moverHaciaDerecha(${i})"
+                    class="btn-mover">
+                    ➜
+                </button>   
+            </td>
+      </tr>
+      `;
+    }
+    tabla.innerHTML = contenido;
 }
