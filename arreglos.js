01136e958033f9c5b95231f8d6a05bf150628f60
paulveloz28 <paulveloz28@gmail.com>
2026-05-03 19:52:36 -0500

    feat: create a function agregarEdad

--- arreglos.js
+++ arreglos.js
@@ -2,3 +2,18 @@
 let edadesIzquierda = [];
 let edadesDerecha = [];
 
+function agregarEdad() {
+
+    // obtiene el valor de la caja de texto
+    let cajaEdad = document.getElementById("edad");
+    let valor = cajaEdad.value;
+    
+    // convierte a entero
+    let edad = parseInt(valor);
+
+    // agrega la edad al arreglo izquierdo
+    edadesIzquierda.push(edad);
+
+    // vuelve a pintar el arreglo izquierdo
+    pintarArregloIzquierdo();
+}
